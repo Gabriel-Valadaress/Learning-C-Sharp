8205ad8c16048c0fef64499f1f5308c6017bca82
Gabriel-Valadaress <gabrieloliveiravaladares@gmail.com>
2025-05-23 09:10:03 -0300

    Add exercises on DateTime usage

---
+++ 5-DateTime/FirstClass_Basics/FirstClass_Basics/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FirstClass_Basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date1 = new DateTime();
+            Console.WriteLine($"Blank date: {date1}");
+
+            DateTime date2 = DateTime.Now;
+            Console.WriteLine($"Current date: {date2}");          
+            Console.WriteLine($"Ticks - How date is storage: {date1.Ticks}\nTicks - How date is storage: {date2.Ticks}");
+
+            DateTime date3 = DateTime.UtcNow;
+            Console.WriteLine($"Current UTC date: {date3}");
+
+            string stringDate = "2025-05-07 15:41:27";
+            DateTime date4 = DateTime.Parse(stringDate);
+            Console.WriteLine($"Date from string: {date4}");
+            Console.WriteLine($"Year: {date4.Year}\nMonth: {date4.Month}\nDay: {date4.Day}\nHours: {date4.Hour}\nMinutes: {date4.Minute}\nSeconds: {date4.Second}\nDay of week: {date4.DayOfWeek}\nDay of year: {date4.DayOfYear}");
+
+            DateTime finish = DateTime.Now;
+            Console.WriteLine($"Current date: {finish}");
+            Console.WriteLine($"Execution time: {(finish - date2).TotalSeconds} seconds or {(finish - date2).TotalMilliseconds} miliseconds");
+        }
+    }
+}
---
+++ 5-DateTime/SecondClass_Operations/SecondClass_Operations/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SecondClass_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date = new DateTime(2025, 5, 7);
+            Console.WriteLine($"Date: {date}");
+
+            date = date.AddSeconds(10);
+            Console.WriteLine($"Date + 10s: {date}");
+
+            date = date.AddMinutes(10);
+            Console.WriteLine($"Date + 10min: {date}");
+
+            date = date.AddHours(10);
+            Console.WriteLine($"Date + 10h: {date}");
+
+            date = date.AddDays(10);
+            Console.WriteLine($"Date + 10d: {date}");
+
+            date = date.AddMonths(10);
+            Console.WriteLine($"Date + 10mth: {date}");
+
+            date = date.AddYears(10);
+            Console.WriteLine($"Date + 10y: {date}");
+        }
+    }
+}
