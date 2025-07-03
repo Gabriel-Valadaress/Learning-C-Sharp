d16f797ce929fcd9fbb202985b70b7e5d2980bd8
Gabriel-Valadaress <gabrieloliveiravaladares@gmail.com>
2025-07-03 16:16:13 -0300

    finished classes and initiate final project: console chess

---
+++ 13-Lambda-Delegates-LINQ/Linq04_SQLSintax/Linq04_SQLSintax/Entities/Product.cs
@@ -0,0 +1,23 @@
+﻿namespace Linq04_SQLSintax.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public Category Category { get; set; }
+
+        public Product(int id, string name, double price, Category category)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Category = category;
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {Id}, Name: {Name}, Price: ${Price.ToString("F2")}, Category: {Category.Name}, Tier: {Category.Tier}";
+        }
+    }
+}
